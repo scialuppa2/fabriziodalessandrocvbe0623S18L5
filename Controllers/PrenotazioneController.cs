28ed3a1c8b4a3d71a1b2b17ddf635c801a39d827
scialuppa2 <148256332+scialuppa2@users.noreply.github.com>
2024-03-09 12:47:47 +0100

    Chiamate asincrone

--- Controllers/PrenotazioneController.cs
+++ Controllers/PrenotazioneController.cs
@@ -1,7 +1,11 @@
-﻿using progetto_settimanaleS18L5.Models;
+﻿using Newtonsoft.Json;
+using progetto_settimanaleS18L5.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,6 +13,11 @@ namespace progetto_settimanaleS18L5.Controllers
 {
     public class PrenotazioneController : Controller
     {
+        public static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        }
+
         public ActionResult CreaPrenotazione(string CodiceFiscale)
         {
             List<Cliente> listaClienti = new Cliente().ListaClienti();
@@ -58,11 +67,15 @@ namespace progetto_settimanaleS18L5.Controllers
         }
 
         [HttpGet]
-        public ActionResult ListaPrenotazione()
+        public async Task<ActionResult> ListaPrenotazione()
         {
-            var prenotazione = new Prenotazione().ListaPrenotazioni();
-            return View(prenotazione);
+            var prenotazioni = new Prenotazione().ListaPrenotazioni();
+
+            ViewBag.TotalePensioneCompleta = await ConteggioPensioneCompleta();
+
+            return View(prenotazioni);
         }
+
 
         public ActionResult EliminaPrenotazione(int IdPrenotazione)
         {
@@ -74,5 +87,69 @@ namespace progetto_settimanaleS18L5.Controllers
             }
             return RedirectToAction("ListaPrenotazione");
         }
+
+        public ActionResult RicercaPrenotazione()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> RicercaPrenotazioneCliente(string CodiceFiscale)
+        {
+            List<dynamic> prenotazioni = new List<dynamic>();
+
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString))
+            {
+                await connection.OpenAsync();
+
+                string query = "SELECT IdPrenotazione, NumeroCamera, DataInizioSoggiorno, DataFineSoggiorno, TariffaApplicata FROM Prenotazioni WHERE CodiceFiscale = @CodiceFiscale";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@CodiceFiscale", CodiceFiscale);
+
+                    using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            prenotazioni.Add(new
+                            {
+                                IdPrenotazione = reader.GetInt32(reader.GetOrdinal("IdPrenotazione")),
+                                NumeroCamera = reader.GetInt32(reader.GetOrdinal("NumeroCamera")),
+                                DataInizioSoggiorno = reader.GetDateTime(reader.GetOrdinal("DataInizioSoggiorno")).ToString("yyyy-MM-dd"),
+                                DataFineSoggiorno = reader.GetDateTime(reader.GetOrdinal("DataFineSoggiorno")).ToString("yyyy-MM-dd"),
+                                TariffaApplicata = reader.GetDecimal(reader.GetOrdinal("TariffaApplicata"))
+                            });
+                        }
+                    }
+                }
+            }
+
+            return Json(prenotazioni, JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<string> ConteggioPensioneCompleta()
+        {
+            int conteggio = 0;
+
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString))
+            {
+                await connection.OpenAsync();
+
+                string query = "SELECT COUNT(*) FROM Prenotazioni WHERE PensioneCompleta = 1";
+
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    conteggio = (int)await command.ExecuteScalarAsync();
+                }
+            }
+
+            return conteggio.ToString();
+        }
+
+
+
+
+
     }
 }
